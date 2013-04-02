78f5d4d6126b92bbd811155f90f090a227b8894b
William Denton <wtd@pobox.com>
2013-04-01 23:20:43 -0400

    drop-layer.rb now takes an argument

--- drop-layer.rb
+++ drop-layer.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
-require 'cgi'
 
 require 'rubygems'
 require 'active_record'
+
+layer = ARGV[0]
+if layer.nil?
+  puts "Please specify a layer to remove"
+  exit
+end
 
 dbconfig = YAML::load(File.open('config/database.yml'))[ENV['ENV'] ? ENV['ENV'] : 'development']
 ActiveRecord::Base.establish_connection(dbconfig)
 
 Dir.glob('./app/models/*.rb').each { |r| require r }
 
-
-l = Layer.find_or_create_by_name(:name => "claxton",
+l = Layer.find_or_create_by_name(:name => layer,
                             :refreshInterval => 300,
                             :refreshDistance => 100,
                             :fullRefresh => true,
@@ -20,7 +24,7 @@ l = Layer.find_or_create_by_name(:name => "claxton",
                             :biwStyle => "classic",
                             )
 
-puts l.name
+puts "Found #{l.name} ... deleting"
 
 # With has_and_belongs_to_many and has_many :through, if you want to delete the associated records themselves, you can always do something along the lines of person.tasks.each(&:destroy).
 
