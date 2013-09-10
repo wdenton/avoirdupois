ed96d9e04a18d9eac74a804adb4ab57989bb7f4f
William Denton <wtd@pobox.com>
2013-09-09 22:05:21 -0400

    I think it works

--- README.md
+++ README.md
@@ -3,11 +3,100 @@ Avoirdupois
 
 Point of interest (POI) provider for Layer, written in Ruby.
 
+## Requirements
+
+Avoirdupois is written in [Ruby](http://www.ruby-lang.org/en/) using the [Sinatra](http://www.sinatrarb.com/) web application framework.  It uses ActiveRecord from Ruby on Rails to talk to the database, but this will probably change.
+
+You will need to have Ruby and [Rubygems](http://rubygems.org/) installed for this to work.  You can either do that through a package manager or by using [RVM](https://rvm.io/), which is probably easiest in the long run and will help you avoid problems with different Ruby versions.
+
+You will also need [Git](http://git-scm.com/) to get this source code.
+
+    $ sudo apt-get install git
+
+While I'm at it, let me recommend two other useful tools: [curl](http://curl.haxx.se/) and [jsonlint](https://github.com/zaach/jsonlint).
+
+    $ sudo apt-get install curl
+    $ sudo apt-get install npm
+	$ npm install jsonlint -g
+
+All of these installation commands are meant for Debian/Ubuntu; adapt them to your system if you run something different.
+
 ## Installation
 
-To be done.
+To install Laertes you need to get this source code by either forking this GitHub repository or downloading it directly.  Then use [Bundler](http://gembundler.com/) to first make sure all of the necessary requirements are in place and then to run the application safely.  (Note: when installing Bundler, if you're not using RVM you may need to run `sudo gem install bundler`.)  This will clone this repository and then get it running:
+
+    $ git clone git@github.com:wdenton/avoirdupois.git
+    $ cd avoirdupois
+    $ gem install bundler
+    $ bundle install
+    $ bundle exec rackup config.ru
+
+You should now see a message like this:
+
+    [2013-01-22 10:49:56] INFO  WEBrick 1.3.1
+    [2013-01-22 10:49:56] INFO  ruby 1.9.3 (2012-04-20) [x86_64-linux]
+    [2013-01-22 10:49:56] INFO  WEBrick::HTTPServer#start: pid=14347 port=9292
+
+Good! This means that the web service is running on your machine on port 9292.  You can now test it by either hitting it on the command line or in your browser with a URL like this:
+
+    $ curl "http://localhost:9292/?layerName=sample&lon=-79.4&lat=43.6&version=6.2&radius=2000"
+
+You'll get an error because there is no such layer 'sample' yet:
+
+    {
+      "errorCode": 22,
+      "errorString": "No such layer sample"
+    }
+
+To create the sample layer, run
+
+	$ ./loaders/loader.rb loaders/sample/sample.yaml
+
+It should respond by showing the sample data it is loading in (some points of interest in Toronto, Ontario):
+
+    Queen's Park
+      Action: Wikipedia entry
+      Icon: Queen's Park
+      Checkbox: park
+    Royal Ontario Museum
+      Icon: Royal Ontario Museum
+      Checkbox: museum
+    Checkbox configuration for Layar:
+    1 | park
+    2 | museum
+
+Now rerun the request:
+
+    $ curl "http://localhost:9292/?layerName=sample&lon=-79.4&lat=43.6&version=6.2&radius=2000"
+
+It should respond with JSON output (as defined in Layar's [GetPOIs Response](https://www.layar.com/documentation/browser/api/getpois-response/)). As long as there is some JSON, even if it's not much, that's good.  If there's an error, look at your console to see what it might be.
+
+If you installed `jsonlint` then this will make the output more readable:
+
+    $ curl "http://localhost:9292/?layerName=sample&lon=-79.4&lat=43.6&version=6.2&radius=2000" | jsonlint
+    {
+      "layer": "sample",
+      "showMessage": "This is a sample layer in Layar.",
+      "refreshDistance": 100,
+      "refreshInterval": 300,
+      "hotspots": [
+        {
+          "id": 1,
+          "text": {
+            "title": "Queen's Park",
+            "description": "An urban park, and home to the Ontario Legislative Building.",
+            "footnote": "Footnote here."
+          },
+          "anchor": {
+            "geolocation": {
+              "lat": 43.6646,
+              "lon": -79.3926
+            }
+          },
+    [ ... and a lot more ... ]
 
 ## Loading in POIS
 
 To be done.
+
 
--- avoirdupois.rb
+++ avoirdupois.rb
@@ -76,90 +76,91 @@ get "/" do
   errorcode = 0
   errorstring = "ok"
 
-  @layer = Layer.find_by_name(params[:layerName])
+  layer = Layer.find_by name: params[:layerName]
 
-  # Exit cleanly right away if the requested layer isn't known
-  if @layer.nil?
-    response = {
-      "errorCode"   => 20,
-      "errorString" => "No such layer " + params["layerName"][0]
-    }
-    puts response.to_json
-    exit
-  end
+  if layer
 
-latitude  = params["lat"][0].to_f
-longitude = params["lon"][0].to_f
+    logger.debug "Found layer #{layer.name}"
+    logger.debug layer
+    logger.debug layer.pois
 
-radius = params["radius"][0].to_i || 1000 # Default to 1000m radius if none provided
+    latitude  = params[:lat].to_f
+    longitude = params[:lon].to_f
 
-# Turn CHECKBOXLIST=1,2,5 into array of integers [1, 2, 5].
-# This gets a bit ugly later.
-if params["CHECKBOXLIST"].size > 0
-  checkmarks = params["CHECKBOXLIST"][0].split(",").map {|x| x.to_i}
-else
-  checkmarks = []
-end
+    radius = params[:radius].to_i || 1000 # Default to 1000m radius if none provided
 
-hotspots = []
+    logger.debug "Latitude: #{latitude}"
+    logger.debug "Longitude: #{longitude}"
+    logger.debug "Radius: #{radius}"
 
-# TODO: Add support for nextPageKey
-# TODO: Add support for more Pages
-# TODO: Add support for layer-level actions
-# TODO: Add support for deletedHotspots
-# TODO: Add support for animations
-# http://layar.com/documentation/browser/api/getpois-response/
+    # Turn CHECKBOXLIST=1,2,5 into array of integers [1, 2, 5].
+    # This gets a bit ugly later.
+    if params[:CHECKBOXLIST] # .size > 0
+      checkmarks = params[:CHECKBOXLIST].split(",").map {|x| x.to_i}
+      logger.debug "Checkbox list: #{checkmarks}"
+    else
+      checkmarks = []
+    end
 
-# Find all of the POIs in range in this layer.
-#
-# There's a slightly ugly SQL statement here that's used with a
-# find_by_sql statement because we can't use the ActiveRecord methods
-# to do exactly what we want: determining the distances between the
-# user and the POIs.  We need to use the Haversine formula for this.
-# In the SQL statement we do a calculation (thanks to MySQL having all
-# of this built in) and then assign that number to the variable
-# distance, then select and sort based on distance.  It would be nice
-# if we could use ActiveRecord normally to do this, with some sort of
-# class method on Poi, but we can't, because there's no way to get
-# "as" into the statement.
-#
-# If we didn't need to bother so much about distance, we could just do
-# a query like this:
-#
-# @layer.pois.group(:id).checkboxed(checkmarks).each do |poi|
-#   next unless poi.within_radius(latitude, longitude, radius)
-#   puts poi.title
-# end
-#
-# That works fine.  See poi.rb for the checkboxed method, with uses
-# ActiveRecord's join and where commands to control the SQL the way we
-# want.
-#
-# If there really is some way to say
-# @layer.pois.group(:id).within_range(latitude, longitude, radius)
-# then we definitely want to use it.
+    hotspots = []
 
-# Note re tests: make sure the id numbers returned are unique.
-# Not specifying IDs from the pois table will lead to trouble.
+    # TODO: Add support for nextPageKey
+    # TODO: Add support for more Pages
+    # TODO: Add support for layer-level actions
+    # TODO: Add support for deletedHotspots
+    # TODO: Add support for animations
+    # http://layar.com/documentation/browser/api/getpois-response/
 
-# All right, this stuff about checkboxes is a bit ugly.
-#
-# If there are no checkboxes given for a layer, then we don't want to
-# use them in our SQL query because it won't work.  So we need to have
-# two different SQL queries ready, one for each case.
+    # Find all of the POIs in range in this layer.
+    #
+    # There's a slightly ugly SQL statement here that's used with a
+    # find_by_sql statement because we can't use the ActiveRecord methods
+    # to do exactly what we want: determining the distances between the
+    # user and the POIs.  We need to use the Haversine formula for this.
+    # In the SQL statement we do a calculation (thanks to MySQL having all
+    # of this built in) and then assign that number to the variable
+    # distance, then select and sort based on distance.  It would be nice
+    # if we could use ActiveRecord normally to do this, with some sort of
+    # class method on Poi, but we can't, because there's no way to get
+    # "as" into the statement.
+    #
+    # If we didn't need to bother so much about distance, we could just do
+    # a query like this:
+    #
+    # @layer.pois.group(:id).checkboxed(checkmarks).each do |poi|
+    #   next unless poi.within_radius(latitude, longitude, radius)
+    #   puts poi.title
+    # end
+    #
+    # That works fine.  See poi.rb for the checkboxed method, with uses
+    # ActiveRecord's join and where commands to control the SQL the way we
+    # want.
+    #
+    # If there really is some way to say
+    # @layer.pois.group(:id).within_range(latitude, longitude, radius)
+    # then we definitely want to use it.
+    
+    # Note re tests: make sure the id numbers returned are unique.
+    # Not specifying IDs from the pois table will lead to trouble.
+    
+    # All right, this stuff about checkboxes is a bit ugly.
+    #
+    # If there are no checkboxes given for a layer, then we don't want to
+    # use them in our SQL query because it won't work.  So we need to have
+    # two different SQL queries ready, one for each case.
 
-if @layer.has_checkboxes
-  # This layer has some, so we need a more complicated query.
+    if layer.has_checkboxes
+      # This layer has some, so we need a more complicated query.
+      
+      # When no checkboxes are selected, return nothing with
+      # "c.option_value in (NULL) "
+      checkmarks = "NULL" if checkmarks.empty?
+      
+      # When no checkboxes are selected, assume it's an oversight
+      # and return all POIs in range.
+      # checkmarks = known_checkboxes if checkmarks.empty?
 
-  # When no checkboxes are selected, return nothing with
-  # "c.option_value in (NULL) "
-  checkmarks = "NULL" if checkmarks.empty?
-
-  # When no checkboxes are selected, assume it's an oversight
-  # and return all POIs in range.
-  # checkmarks = known_checkboxes if checkmarks.empty?
-
-  sql = "SELECT p.*,
+      sql = "SELECT p.*,
  (((acos(sin((? * pi() / 180)) * sin((lat * pi() / 180)) +  cos((? * pi() / 180)) * cos((lat * pi() / 180)) * cos((? - lon) * pi() / 180))) * 180 / pi())* 60 * 1.1515 * 1.609344 * 1000) AS distance
  FROM  pois p
  INNER JOIN checkboxes_pois cp ON cp.poi_id = p.id
@@ -169,130 +170,152 @@ if @layer.has_checkboxes
  GROUP BY p.id
  HAVING distance < ?
  ORDER BY distance asc" # "
-  pois = Poi.find_by_sql([sql, latitude, latitude, longitude, @layer.id, checkmarks, radius])
-else
-  # We can do a simpler query.
-  sql = "SELECT p.*,
+      pois = Poi.find_by_sql([sql, latitude, latitude, longitude, layer.id, checkmarks, radius])
+
+    else
+
+      # We can do a simpler query.
+      sql = "SELECT p.*,
  (((acos(sin((? * pi() / 180)) * sin((lat * pi() / 180)) +  cos((? * pi() / 180)) * cos((lat * pi() / 180)) * cos((? - lon) * pi() / 180))) * 180 / pi())* 60 * 1.1515 * 1.609344 * 1000) AS distance
  FROM  pois p
  WHERE p.layer_id = ?
  GROUP BY p.id
  HAVING distance < ?
  ORDER BY distance asc" # "
-  pois = Poi.find_by_sql([sql, latitude, latitude, longitude, @layer.id, radius])
-end
-
-pois.each do |poi|
-  # TODO: Add paging through >50 results.
-  # STDERR.puts poi.title
-  hotspot = Hash.new
-  hotspot["id"] = poi.id
-  hotspot["text"] = {
-    "title"       => poi.title,
-    "description" => poi.description,
-    "footnote"    => poi.footnote
-  }
-  hotspot["anchor"]         = {"geolocation" => {"lat" => poi.lat, "lon" => poi.lon}}
-  hotspot["imageURL"]       = poi.imageURL
-  hotspot["biwStyle"]       = poi.biwStyle
-  hotspot["showSmallBiw"]   = poi.showSmallBiw
-  hotspot["showBiwOnClick"] = poi.showBiwOnClick
-
-  if poi.actions
-    hotspot["actions"] = []
-    poi.actions.each do |action|
-      # STDERR.puts action["label"]
-      hotspot["actions"] << {
-        "uri"          => action.uri,
-        "label"        => action.label,
-        "contentType"  => action.contentType,
-        "activityType" => action.activityType,
-        "method"       => action.method
-      }
-    end
-  end
-
-  if poi.icon
-    hotspot["icon"] = {
-      "url"  => poi.icon.url,
-      "type" => poi.icon.iconType
-    }
-  end
-
-  if poi.ubject # Object being a reserved word in Ruby
-    hotspot["object"] = {
-      # These three are common to both kinds of objects.
-      "url"         => poi.ubject.url,
-      "contentType" => poi.ubject.contentType,
-      "size"        => poi.ubject.size
-    }
-    if poi.ubject.contentType == "model/vnd.layar.l3d"
-      hotspot["object"]["reducedURL"] = poi.ubject.reducedURL
-    elsif poi.ubject.contentType == "text/html"
-      hotspot["object"]["viewport"] = {
-        "width"       => poi.ubject.width,
-        "height"      => poi.ubject.height,
-        "scrollable"  => poi.ubject.scrollable,
-        "interactive" => poi.ubject.interactive
-      }
+      pois = Poi.find_by_sql([sql, latitude, latitude, longitude, layer.id, radius])
     end
 
+    logger.debug "Found #{pois.size} POIs"
 
-  end
+    # pois = layer.pois
 
-  # TODO Test a transform
-  if poi.transform
-    # STDERR.puts poi.transform.url
-    hotspot["transform"] = {
-      "rotate" => {
-        "rel"   => poi.transform.rel,
-        "angle" => poi.transform.angle,
-        "axis" => {
-          "x" => poi.transform.rotate_x,
-          "y" => poi.transform.rotate_y,
-          "z" => poi.transform.rotate_z,
+    pois.each do |poi|
+      # TODO: Add paging through >50 results.
+      # STDERR.puts poi.title
+      hotspot = Hash.new
+      hotspot["id"] = poi.id
+      hotspot["text"] = {
+        "title"       => poi.title,
+        "description" => poi.description,
+        "footnote"    => poi.footnote
+      }
+      hotspot["anchor"]         = {"geolocation" => {"lat" => poi.lat, "lon" => poi.lon}}
+      hotspot["imageURL"]       = poi.imageURL
+      hotspot["biwStyle"]       = poi.biwStyle
+      hotspot["showSmallBiw"]   = poi.showSmallBiw
+      hotspot["showBiwOnClick"] = poi.showBiwOnClick
+
+      logger.debug "Hotspot #{poi.id}: #{poi.title}"
+
+      if poi.actions
+        hotspot["actions"] = []
+        poi.actions.each do |action|
+          # STDERR.puts action["label"]
+          hotspot["actions"] << {
+            "uri"          => action.uri,
+            "label"        => action.label,
+            "contentType"  => action.contentType,
+            "activityType" => action.activityType,
+            "method"       => action.method
+          }
+        end
+      end
+      
+      if poi.icon
+        hotspot["icon"] = {
+          "url"  => poi.icon.url,
+          "type" => poi.icon.iconType
         }
-      },
-      "translate" => {
-        "x" => poi.transform.translate_x,
-        "y" => poi.transform.translate_y,
-        "z" => poi.transform.translate_z,
-      },
-    "scale" => poi.transform.scale
+      end
+      
+      if poi.ubject # Object being a reserved word in Ruby
+        hotspot["object"] = {
+          # These three are common to both kinds of objects.
+          "url"         => poi.ubject.url,
+          "contentType" => poi.ubject.contentType,
+          "size"        => poi.ubject.size
+        }
+        if poi.ubject.contentType == "model/vnd.layar.l3d"
+          hotspot["object"]["reducedURL"] = poi.ubject.reducedURL
+        elsif poi.ubject.contentType == "text/html"
+          hotspot["object"]["viewport"] = {
+            "width"       => poi.ubject.width,
+            "height"      => poi.ubject.height,
+            "scrollable"  => poi.ubject.scrollable,
+            "interactive" => poi.ubject.interactive
+          }
+        end
+        
+      end
+
+      # TODO Test a transform
+      if poi.transform
+        # STDERR.puts poi.transform.url
+        hotspot["transform"] = {
+          "rotate" => {
+            "rel"   => poi.transform.rel,
+            "angle" => poi.transform.angle,
+            "axis" => {
+              "x" => poi.transform.rotate_x,
+              "y" => poi.transform.rotate_y,
+              "z" => poi.transform.rotate_z,
+            }
+          },
+          "translate" => {
+            "x" => poi.transform.translate_x,
+            "y" => poi.transform.translate_y,
+            "z" => poi.transform.translate_z,
+          },
+          "scale" => poi.transform.scale
+        }
+      end
+      
+      hotspots << hotspot
+    end
+    
+    if hotspots.length == 0
+      errorcode = 21
+      errorstring = "No results found.  Try adjusting your search range and any filters."
+      # TODO Make error message customizable?
+    end
+
+    response = {
+      "layer"           => layer.name,
+      "showMessage"     => layer.showMessage, # + " (#{ENV['ENV']})",
+      "refreshDistance" => layer.refreshDistance,
+      "refreshInterval" => layer.refreshInterval,
+      "hotspots"        => hotspots,
+      "errorCode"       => errorcode,
+      "errorString"     => errorstring,
     }
-  end
+    # TODO add layer actions
+    
+    # "NOTE that this parameter must be returned if the GetPOIs request
+    # doesn't contain a requested radius. It cannot be used to overrule a
+    # value of radius if that was provided in the request. the unit is
+    # meter."
+    # -- http://layar.com/documentation/browser/api/getpois-response/#root-radius
+    if ! params["radius"]
+      response["radius"] = radius
+    end
 
-  hotspots << hotspot
-end
+  else # The requested layer is not known, so return an error
 
-if hotspots.length == 0
-  errorcode = 21
-  errorstring = "No results found.  Try adjusting your search range and any filters."
-  # TODO Customize the error message
-end
+    errorcode = 22
+    errorstring = "No such layer #{params[:layerName]}"
+    response = {
+      "layer"           => params[:layerName],
+      # "refreshDistance" => 300,
+      # "refreshInterval" => 100,
+      "hotspots"        => [],
+      "errorCode"       => errorcode,
+      "errorString"     => errorstring,
+    }
+    logger.error errorstring
 
-response = {
-  "layer"           => @layer.name,
-  "showMessage"     => @layer.showMessage, # + " (#{ENV['ENV']})",
-  "refreshDistance" => @layer.refreshDistance,
-  "refreshInterval" => @layer.refreshInterval,
-  "hotspots"        => hotspots,
-  "errorCode"       => errorcode,
-  "errorString"     => errorstring,
-}
-# TODO add layer actions
+   end
 
-# "NOTE that this parameter must be returned if the GetPOIs request
-# doesn't contain a requested radius. It cannot be used to overrule a
-# value of radius if that was provided in the request. the unit is
-# meter."
-# -- http://layar.com/documentation/browser/api/getpois-response/#root-radius
-if ! params["radius"]
-  response["radius"] = radius
-end
-
-puts "Content-type: application/json"
-puts
-puts response.to_json
+  response.to_json
 
 end
+  
